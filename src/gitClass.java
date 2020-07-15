a0a60d9978b6e5f83d8089e13ac2ebd1811f84ad
SarithaCherry <SarithaCherry@HpPavillionn1>
2020-07-15 21:29:14 +0530

    first commit

---
+++ src/gitClass.java
@@ -0,0 +1,15 @@
+
+public class gitClass {
+	public static void main (String[]args) {
+		String hungry = "yes";
+		
+		if (hungry=="yes") 
+			System.out.println("I am hungry"); 
+			else {
+				System.out.println("I am not hungry");}
+			
+			}
+		}
+	
+
+
